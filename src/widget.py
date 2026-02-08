7d6e4d6913a0d1d83c3490a6704f1d71187c617f
Gregory Salnikov <greengo86@mail.ru>
2026-02-09 00:25:53 +0400

    add new func get_date

--- src/widget.py
+++ src/widget.py
@@ -18,3 +18,17 @@ def mask_account_card(card_or_scor: str) -> str:
         card_number = int(card_or_scor.split()[-1])
         return get_mask_card_number(card_number)
 
+
+def get_date(date_information: str) -> str:
+    """
+    Преобразует формат даты,
+    :param date_information: принимает на вход строку с датой в формате
+    '2024-03-11T02:26:18.671407'
+    :return: возвращает строку с датой в формате 'ДД.ММ.ГГГГ' ('11.03.2024')
+    """
+    # Получение всех групп в виде кортежа
+    date_pattern = r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})"
+    match = re.search(date_pattern, date_information)
+    if match:
+        year, month, day = match.groups()
+    return f"{day}.{month}.{year}"
