43eb6f58d327e69bfbcad0e3444ad51ad836f54d
Gregory Salnikov <greengo86@mail.ru>
2026-03-31 13:05:07 +0400

    added docstring to finance_file_reader

--- src/finance_file_reader.py
+++ src/finance_file_reader.py
@@ -2,10 +2,12 @@ import pandas as pd
 
 
 def read_csv_transactions(path: str) -> list[dict]:
+    """Читает транзакции из csv-файла и возвращает их в виде списка словарей."""
     df = pd.read_csv(path, delimiter=';')
     return df.to_dict(orient='records')
 
 
 def read_excel_transactions(path: str) -> list[dict]:
+    """Читает транзакции из Excel-файла и возвращает их в виде списка словарей."""
     df = pd.read_excel(path)
     return df.to_dict(orient='records')
