1850f75ff6b8f07daaf7d956508d61239596f10d
Gregory Salnikov <greengo86@mail.ru>
2026-03-31 15:22:20 +0400

    fixed Readme.md

--- src/finance_file_reader.py
+++ src/finance_file_reader.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 def read_csv_transactions(path: str) -> list[dict]:
     """Читает транзакции из csv-файла и возвращает их в виде списка словарей."""
-    df = pd.read_csv(path, delimiter=';')
-    return df.to_dict(orient='records')
+    df = pd.read_csv(path, delimiter=";")
+    return df.to_dict(orient="records")
 
 
 def read_excel_transactions(path: str) -> list[dict]:
     """Читает транзакции из Excel-файла и возвращает их в виде списка словарей."""
     df = pd.read_excel(path)
-    return df.to_dict(orient='records')
+    return df.to_dict(orient="records")
